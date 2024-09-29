4e7793e695007d4619b32947fdbb0947773efff5
Luara Raggio <luararaggio@hotmail.com>
2024-09-29 00:45:36 -0300

    atualizando organizacao da gnl

--- test/main.c
+++ test/main.c
@@ -14,13 +14,12 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-    int fd;
-    char *s;
-
-   fd = open("teste.txt", O_RDONLY);
-   s = get_next_line(fd);
-   printf("%s", s);
-   free(s);
+	int fd;
+	char *s;
+ 	s = get_next_line(fd);
+	fd = open("teste.txt", O_RDONLY);
+	printf("%s", s);
+	free(s);
 }
